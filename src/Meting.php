3c5809cfc33b3aaddf1fd07d98384086efd848be
liwanglin12 <i@lwl12.com>
2018-01-02 21:03:00 +0800

    Update Meting.php

--- src/Meting.php
+++ src/Meting.php
@@ -3,7 +3,7 @@
  * Meting music framework
  * https://i-meto.com
  * https://github.com/metowolf/Meting
- * Version 1.4.0
+ * Version 1.3.10
  *
  * Copyright 2017, METO Sheel <i@i-meto.com>
  * Released under the MIT license
