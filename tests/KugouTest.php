a215437d24c544c92f67ee0c6a97da39f2f70370
METO <i@i-meto.com>
2018-03-01 11:43:55 +0000

    Apply fixes from StyleCI

--- tests/KugouTest.php
+++ tests/KugouTest.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Metowolf\Meting;
+
 use Metowolf\Meting;
 use PHPUnit\Framework\TestCase;
 
@@ -9,35 +11,39 @@ class KugouTest extends TestCase
     {
         $api = new Meting('kugou');
         $data = $api->format(true)->search('hello');
-        $data = json_decode($data,1);
+        $data = json_decode($data, 1);
         $this->assertNotEmpty($data);
     }
 
-    public function testSong(){
+    public function testSong()
+    {
         $api = new Meting('kugou');
         $data = $api->format(true)->song('9ed11b0c5a86f33541fa88b57c5ba00a');
-        $data = json_decode($data,1);
+        $data = json_decode($data, 1);
         $this->assertNotEmpty($data);
     }
 
-    public function testPic(){
+    public function testPic()
+    {
         $api = new Meting('kugou');
         $data = $api->format(true)->pic('9ed11b0c5a86f33541fa88b57c5ba00a');
-        $data = json_decode($data,1);
+        $data = json_decode($data, 1);
         $this->assertNotEmpty($data);
     }
 
-    public function testUrl(){
+    public function testUrl()
+    {
         $api = new Meting('kugou');
         $data = $api->format(true)->url('9ed11b0c5a86f33541fa88b57c5ba00a');
-        $data = json_decode($data,1);
+        $data = json_decode($data, 1);
         $this->assertNotEmpty($data);
     }
 
-    public function testLyric(){
+    public function testLyric()
+    {
         $api = new Meting('kugou');
         $data = $api->format(true)->lyric('9ed11b0c5a86f33541fa88b57c5ba00a');
-        $data = json_decode($data,1);
+        $data = json_decode($data, 1);
         $this->assertNotEmpty($data);
     }
 }
