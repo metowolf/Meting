92148453fc7c40bfeb3d150567fb015023a5f7cf
metowolf <i@i-meto.com>
2021-06-27 16:03:20 +0000

    release: 1.5.10

--- src/Meting.php
+++ src/Meting.php
@@ -3,7 +3,7 @@
  * Meting music framework
  * https://i-meto.com
  * https://github.com/metowolf/Meting
- * Version 1.5.9.
+ * Version 1.5.10.
  *
  * Copyright 2019, METO Sheel <i@i-meto.com>
  * Released under the MIT license
@@ -13,7 +13,7 @@ namespace Metowolf;
 
 class Meting
 {
-    const VERSION = '1.5.9';
+    const VERSION = '1.5.10';
 
     public $raw;
     public $data;
